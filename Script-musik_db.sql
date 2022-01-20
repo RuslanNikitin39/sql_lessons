e6669657bd3a31ab154eab9a95369498856fe489
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2022-01-20 10:57:31 +0200

    create first request

---
+++ Script-musik_db.sql
@@ -0,0 +1,27 @@
+create table if not exists genres (
+	id serial primary key,
+	name varchar(50) not null unique
+);
+
+create table if not exists singers (
+	id serial primary key,
+	name varchar(150) not null unique,
+	genre_id integer references genres(id)
+);
+
+create table if not exists albums (
+	id serial primary key,
+	name varchar(150) not null unique,
+	release_year integer(4) not null,
+	singer_id integer references singers(id)
+);
+
+create table if not exists tracks (
+	id serial primary key,
+	name varchar(150) not null unique,
+	album_id integer references albums(id),
+	track_length numeric(100) check(track_length > 0)
+);
+
+alter table tracks 
+	alter column track_length set data type numeric(10);
